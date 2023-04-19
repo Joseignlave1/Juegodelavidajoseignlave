033e07e87b1dc6722dfb6f1e12e6038dc1726b09
Joseignlave1 <jose.lavecchia@correo.ucu.edu.uy>
2023-04-19 13:46:38 -0300

    ultimos cambios

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -2,11 +2,31 @@
 
 namespace PII_Game_Of_Life
 {
-    class Program
+    class Program //cumple con el patrón SRP ya que tiene una única responsabilidadz
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Crear instancia de la clase Tablero
+            bool[,] initialBoard = LeerArchivo.LeerTableroDesdeArchivo("board.txt");
+            Tablero tablero = new Tablero(initialBoard);
+
+            // Mostrar contenido del tablero en la consola
+            bool[,] gameBoard = tablero.GetGameBoard();
+            for (int y = 0; y < gameBoard.GetLength(1); y++)
+            {
+                for (int x = 0; x < gameBoard.GetLength(0); x++)
+                {
+                    if (gameBoard[x, y])
+                    {
+                        Console.Write("X ");
+                    }
+                    else
+                    {
+                        Console.Write("_ ");
+                    }
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
